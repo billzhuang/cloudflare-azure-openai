336f9bbef1bc94d0bead4b4256ac9169a89a375b
Bill Zhuang <zhuangmin@gmail.com>
2025-08-11 10:57:29 +0800

    feat: proxy openai requests to azure

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use cloudflare_azure_openai::app;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app()).await.unwrap();
+}
